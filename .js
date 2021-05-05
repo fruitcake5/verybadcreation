39dcc1a3d2f443797a332617eca773398c9bcd0a
fruitcake5 <16598165+fruitcake5@users.noreply.github.com>
2021-05-05 17:47:57 +0100

    Update .js

--- .js
+++ .js
@@ -1,57 +1,55 @@
 const signalR = require("@microsoft/signalr")
 const fetch = require('node-fetch')
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("https://valour.gg/planethub")
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
+
 const config = {
     planetID: ,
-    channelID: ,
-    authToken: ,
-    prefix:
+    authToken: ',
+    prefix: 
 }
 let user = {}
+
+function sendMessage(content, planetID, channelID) {
+    fetch(`https://valour.gg/Channel/PostMessage?token=${config.authToken}`, {
+        "headers": {
+            "content-type": "application/json; charset=utf-8"
+        },
+        "body": JSON.stringify({ author_Id: user.id, channel_Id: channelID, content: content, id: 0, message_Index: 0, planet_Id: planetID, timeSent: new Date().toISOString() }),
+        "method": "POST"
+        })
+}
+
+function message(data) {
+    if (data.Content.toLowerCase().startsWith(`${config.prefix}ping`) || data.Content.toLowerCase().startsWith(`${config.prefix}pong`) || data.Content.toLowerCase().startsWith(`🏓`)) {
+        sendMessage(`**Pong** 🏓 \`${(Date.now()) - (new Date(data.TimeSent).getTime())}ms\``, data.Planet_Id, data.Channel_Id)
+    }
+}
+
 async function start() {
     fetch(`https://valour.gg/User/GetUserWithToken?token=${config.authToken}`)
     .then(res => res.json())
     .then(async json => {
         if (!json.success) throw new Error('Invalid token')
         user = json.data
-        try {
-            await connection.start()
-            await connection.invoke("JoinPlanet", config.planetID, config.authToken)
-            await connection.invoke("JoinChannel", config.channelID, config.authToken)
-        } catch (err) {
-            console.log(err)
-            setTimeout(start, 5000)
-        }
+        fetch(`https://valour.gg/Channel/GetPlanetChannels?planet_id=${config.planetID}&token=${config.authToken}`)
+            .then(res => res.json())
+            .then(async data => {
+                data = data.data
+                data.forEach(async channel => {
+                    const connection = new signalR.HubConnectionBuilder()
+                        .withUrl("https://valour.gg/planethub")
+                        .configureLogging(signalR.LogLevel.Information)
+                        .build();
+                    await connection.start()
+                    await connection.invoke("JoinPlanet", config.planetID, config.authToken)
+                    await connection.invoke("JoinChannel", channel.id, config.authToken)
+                    connection.on("Relay", (data) => {
+                        data = JSON.parse(data)
+                        message(data)
+                    })
+                    connection.onclose(start)
+                })
+            })
     })
 }
-connection.onclose(start)
-function sendMessage(content) {
-    const msg = {
-        author_Id: user.id,
-        channel_Id: config.channelID,
-        content: content,
-        id: 0,
-        message_Index: 0,
-        planet_Id: config.planetID,
-        timeSent: new Date().toISOString()
-    }
-    fetch(`https://valour.gg/Channel/PostMessage?token=${config.authToken}`, {
-        "headers": {
-            "content-type": "application/json; charset=utf-8"
-        },
-        "body": JSON.stringify(msg),
-        "method": "POST"
-        })
-}
-
-connection.on("Relay", (data) => {
-    data = JSON.parse(data)
-    if (data.Content.toLowerCase().startsWith(`${config.prefix}ping`)) {
-        sendMessage(`**Pong** 🏓 \`${(Date.now()) - (new Date(data.TimeSent).getTime())}ms\``)
-    }
-})
 
 start()
